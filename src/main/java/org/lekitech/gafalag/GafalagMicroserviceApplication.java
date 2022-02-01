c67b04022db709510886653eec99df418961dd40
tadzjibov <tadzjibov@users.noreply.github.com>
2022-02-01 15:05:13 +0100

    Feature/db connection prod (#11)
    * Create main.yml
    
    * Update main.yml
    
    * added workflow action
    
    * fixed programmatic db connection for secure production release
    
    * modified main.yml github action with db connection args
    
    * fixed cors issue
    
    Co-authored-by: Enver Eskendarov <41239252+eskendarov@users.noreply.github.com>
    Co-authored-by: eskendarov <envereskendarov@gmail.com>

--- src/main/java/org/lekitech/gafalag/GafalagMicroserviceApplication.java
+++ src/main/java/org/lekitech/gafalag/GafalagMicroserviceApplication.java
@@ -4,19 +4,48 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.sql.DataSource;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.HttpMethod.OPTIONS;
+
 @Slf4j
 @SpringBootApplication
 @EnableJpaRepositories
 public class GafalagMicroserviceApplication extends SpringBootServletInitializer {
+    @Value("${gafalag.db.url}")
+    private String dbUrl;
+    @Value("${gafalag.db.username}")
+    private String dbUsername;
+    @Value("${gafalag.db.password}")
+    private String dbPassword;
+
+    private static final long MAXAGESECS = 3600;
+
+    /**
+     * Loading db connection programmatically for secure connection string passing in production
+     * @return
+     */
+    @Bean
+    public DataSource getDataSource() {
+        log.info("\n===== PASSED DB PARAMS =====\n'" + dbUrl + "'\n'" + dbUsername + "'\n'" + dbPassword + "'");
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(dbUrl);
+        dataSourceBuilder.username(dbUsername);
+        dataSourceBuilder.password(dbPassword);
+        return dataSourceBuilder.build();
+    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -44,7 +73,12 @@ public class GafalagMicroserviceApplication extends SpringBootServletInitializer
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 // TODO Allowed frontend requests (CORS)
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name(), OPTIONS.name())
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(MAXAGESECS);
             }
         };
     }
