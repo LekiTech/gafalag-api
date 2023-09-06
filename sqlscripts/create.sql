bc433b002e91a158e0791fa3712834ff3c259e33
Enver Eskendarov <envereskendarov@gmail.com>
2023-09-06 15:35:43 -0700

    [WIP] deleted unnecessary indexes

--- sqlscripts/create.sql
+++ sqlscripts/create.sql
@@ -72,7 +72,7 @@ CREATE TABLE expression (
     id          UUID PRIMARY KEY     DEFAULT uuid_generate_v4(),
     spelling    VARCHAR     NOT NULL,
     details_id  UUID        NOT NULL,
-    language_id VARCHAR     NOT NULL,
+    language_id VARCHAR(3)  NOT NULL,
     created_at  TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at  TIMESTAMPTZ NOT NULL DEFAULT now()
 );
@@ -89,14 +89,14 @@ CREATE TABLE expression_details (
 CREATE TABLE definition_details (
     id                    UUID PRIMARY KEY     DEFAULT uuid_generate_v4(),
     expression_details_id UUID        NOT NULL,
-    language_id           VARCHAR     NOT NULL,
-    dialect_id            INT         NOT NULL,
+    language_id           VARCHAR(3)  NOT NULL,
+    dialect_id            UUID        NOT NULL,
     created_at            TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at            TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE definition_details_tag (
-    tag_abbr              UUID        NOT NULL,
+    tag_abbr              VARCHAR(10) NOT NULL,
     definition_details_id UUID        NOT NULL,
     created_at            TIMESTAMPTZ NOT NULL DEFAULT now()
 );
@@ -140,9 +140,9 @@ CREATE TABLE example (
     source      VARCHAR,
     translation VARCHAR,
     -- source language id
-    src_lang_id VARCHAR     NOT NULL,
+    src_lang_id VARCHAR(3)  NOT NULL,
     -- translation language id
-    trl_lang_id VARCHAR,
+    trl_lang_id VARCHAR(3),
     -- string combination of source and translation
     raw         VARCHAR     NOT NULL,
     created_at  TIMESTAMPTZ NOT NULL DEFAULT now(),
@@ -158,14 +158,14 @@ CREATE TABLE language (
 );
 
 CREATE TABLE dialect (
-    id          SERIAL PRIMARY KEY,
-    language_id VARCHAR     NOT NULL,
+    id          UUID PRIMARY KEY,
+    language_id VARCHAR(3)  NOT NULL,
     "name"      VARCHAR     NOT NULL UNIQUE,
     created_at  TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at  TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
-CREATE TABLE mediafile (
+CREATE TABLE media_file (
     id            UUID PRIMARY KEY,
     expression_id UUID        NOT NULL,
     mediatype     MEDIA_TYPE  NOT NULL,
@@ -177,12 +177,12 @@ CREATE TABLE mediafile (
 CREATE TABLE expression_relation (
     expression_1_id  UUID        NOT NULL,
     expression_2_id  UUID        NOT NULL,
-    relation_type_id INT         NOT NULL,
+    relation_type_id UUID        NOT NULL,
     created_at       TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE relation_type (
-    id         SERIAL PRIMARY KEY,
+    id         UUID PRIMARY KEY,
     "name"     VARCHAR     NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
@@ -216,7 +216,7 @@ CREATE TABLE etymology (
     id            UUID PRIMARY KEY     DEFAULT uuid_generate_v4(),
     expression_id UUID        NOT NULL,
     language_id   VARCHAR(3)  NOT NULL,
-    dialect_id    INT,
+    dialect_id    UUID,
     description   VARCHAR     NOT NULL,
     created_at    TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at    TIMESTAMPTZ NOT NULL DEFAULT now()
@@ -229,7 +229,7 @@ CREATE TABLE "case" (
     -- question to be answered by given case e.g. Who/What?, Whom/What? etc.
     question    VARCHAR,
     -- refers to a language where given the case belongs to.
-    language_id VARCHAR,
+    language_id VARCHAR(3),
     created_at  TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at  TIMESTAMPTZ NOT NULL DEFAULT now()
 );
@@ -250,66 +250,66 @@ CREATE TABLE declension (
 -- endregion TABLES
 
 -- region ALTER TABLES
-ALTER TABLE "expression"
-    ADD FOREIGN KEY ("details_id") REFERENCES "expression_details" ("id"),
-    ADD FOREIGN KEY ("language_id") REFERENCES "language" ("id");
+ALTER TABLE expression
+    ADD FOREIGN KEY (details_id) REFERENCES expression_details (id),
+    ADD FOREIGN KEY (language_id) REFERENCES language (id);
 
-ALTER TABLE "expression_details"
-    ADD FOREIGN KEY ("source_id") REFERENCES "source" ("id");
+ALTER TABLE expression_details
+    ADD FOREIGN KEY (source_id) REFERENCES source (id);
 
-ALTER TABLE "definition_details"
-    ADD FOREIGN KEY ("expression_details_id") REFERENCES "expression_details" ("id"),
-    ADD FOREIGN KEY ("language_id") REFERENCES "language" ("id"),
-    ADD FOREIGN KEY ("dialect_id") REFERENCES "dialect" ("id");
+ALTER TABLE definition_details
+    ADD FOREIGN KEY (expression_details_id) REFERENCES expression_details (id),
+    ADD FOREIGN KEY (language_id) REFERENCES language (id),
+    ADD FOREIGN KEY (dialect_id) REFERENCES dialect (id);
 
-ALTER TABLE "definition_details_tag"
-    ADD FOREIGN KEY ("tag_abbr") REFERENCES "tag" ("abbreviation"),
-    ADD FOREIGN KEY ("definition_details_id") REFERENCES "definition_details" ("id");
+ALTER TABLE definition_details_tag
+    ADD FOREIGN KEY (tag_abbr) REFERENCES tag (abbreviation),
+    ADD FOREIGN KEY (definition_details_id) REFERENCES definition_details (id);
 
-ALTER TABLE "definition"
-    ADD FOREIGN KEY ("definition_details_id") REFERENCES "definition_details" ("id");
+ALTER TABLE definition
+    ADD FOREIGN KEY (definition_details_id) REFERENCES definition_details (id);
 
-ALTER TABLE "definition_tag"
-    ADD FOREIGN KEY ("tag_abbr") REFERENCES "tag" ("abbreviation"),
-    ADD FOREIGN KEY ("definition_id") REFERENCES "definition" ("id");
+ALTER TABLE definition_tag
+    ADD FOREIGN KEY (tag_abbr) REFERENCES tag (abbreviation),
+    ADD FOREIGN KEY (definition_id) REFERENCES definition (id);
 
-ALTER TABLE "expression_example"
-    ADD FOREIGN KEY ("expression_details_id") REFERENCES "expression_details" ("id"),
-    ADD FOREIGN KEY ("example_id") REFERENCES "example" ("id");
+ALTER TABLE expression_example
+    ADD FOREIGN KEY (expression_details_id) REFERENCES expression_details (id),
+    ADD FOREIGN KEY (example_id) REFERENCES example (id);
 
-ALTER TABLE "definition_example"
-    ADD FOREIGN KEY ("example_id") REFERENCES "example" ("id"),
-    ADD FOREIGN KEY ("definition_details_id") REFERENCES "definition_details" ("id");
+ALTER TABLE definition_example
+    ADD FOREIGN KEY (example_id) REFERENCES example (id),
+    ADD FOREIGN KEY (definition_details_id) REFERENCES definition_details (id);
 
-ALTER TABLE "example"
-    ADD FOREIGN KEY ("src_lang_id") REFERENCES "language" ("id"),
-    ADD FOREIGN KEY ("trl_lang_id") REFERENCES "language" ("id");
+ALTER TABLE example
+    ADD FOREIGN KEY (src_lang_id) REFERENCES language (id),
+    ADD FOREIGN KEY (trl_lang_id) REFERENCES language (id);
 
-ALTER TABLE "dialect"
-    ADD FOREIGN KEY ("language_id") REFERENCES "language" ("id");
+ALTER TABLE dialect
+    ADD FOREIGN KEY (language_id) REFERENCES language (id);
 
-ALTER TABLE "mediafile"
-    ADD FOREIGN KEY ("expression_id") REFERENCES "expression" ("id");
+ALTER TABLE media_file
+    ADD FOREIGN KEY (expression_id) REFERENCES expression (id);
 
-ALTER TABLE "expression_relation"
-    ADD FOREIGN KEY ("expression_1_id") REFERENCES "expression" ("id"),
-    ADD FOREIGN KEY ("expression_2_id") REFERENCES "expression" ("id"),
-    ADD FOREIGN KEY ("relation_type_id") REFERENCES "relation_type" ("id");
+ALTER TABLE expression_relation
+    ADD FOREIGN KEY (expression_1_id) REFERENCES expression (id),
+    ADD FOREIGN KEY (expression_2_id) REFERENCES expression (id),
+    ADD FOREIGN KEY (relation_type_id) REFERENCES relation_type (id);
 
-ALTER TABLE "written_source"
-    ADD FOREIGN KEY ("source_id") REFERENCES "source" ("id");
+ALTER TABLE written_source
+    ADD FOREIGN KEY (source_id) REFERENCES source (id);
 
-ALTER TABLE "etymology"
-    ADD FOREIGN KEY ("expression_id") REFERENCES "expression" ("id"),
-    ADD FOREIGN KEY ("language_id") REFERENCES "language" ("id"),
-    ADD FOREIGN KEY ("dialect_id") REFERENCES "dialect" ("id");
+ALTER TABLE etymology
+    ADD FOREIGN KEY (expression_id) REFERENCES expression (id),
+    ADD FOREIGN KEY (language_id) REFERENCES language (id),
+    ADD FOREIGN KEY (dialect_id) REFERENCES dialect (id);
 
 ALTER TABLE "case"
-    ADD FOREIGN KEY ("language_id") REFERENCES "language" ("id");
+    ADD FOREIGN KEY (language_id) REFERENCES language (id);
 
-ALTER TABLE "declension"
-    ADD FOREIGN KEY ("expression_details_id") REFERENCES "expression_details" ("id"),
-    ADD FOREIGN KEY ("case_id") REFERENCES "case" ("id");
+ALTER TABLE declension
+    ADD FOREIGN KEY (expression_details_id) REFERENCES expression_details (id),
+    ADD FOREIGN KEY (case_id) REFERENCES "case" (id);
 -- endregion ALTER TABLES
 
 -- region FUNCTIONS
@@ -354,9 +354,9 @@ CREATE TRIGGER dialect_updated
     FOR EACH ROW
 EXECUTE PROCEDURE set_current_timestamp();
 
-CREATE TRIGGER mediafile_updated
+CREATE TRIGGER media_file_updated
     BEFORE UPDATE
-    ON mediafile
+    ON media_file
     FOR EACH ROW
 EXECUTE PROCEDURE set_current_timestamp();
 
@@ -416,87 +416,33 @@ EXECUTE PROCEDURE set_current_timestamp();
 -- endregion TRIGGERS
 
 -- region INDEXES
-CREATE INDEX "created_at_index" ON "expression" ("created_at");
-CREATE INDEX "updated_at_index" ON "expression" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "expression_details" ("created_at");
-CREATE INDEX "updated_at_index" ON "expression_details" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "definition_details" ("created_at");
-CREATE INDEX "updated_at_index" ON "definition_details" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "definition_details_tag" ("created_at");
-
-CREATE INDEX "created_at_index" ON "definition" ("created_at");
-CREATE INDEX "updated_at_index" ON "definition" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "definition_tag" ("created_at");
-
-CREATE INDEX "created_at_index" ON "tag" ("created_at");
-CREATE INDEX "updated_at_index" ON "tag" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "expression_example" ("created_at");
-CREATE INDEX "created_at_index" ON "definition_example" ("created_at");
-
-CREATE INDEX "created_at_index" ON "example" ("created_at");
-CREATE INDEX "updated_at_index" ON "example" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "language" ("created_at");
-CREATE INDEX "updated_at_index" ON "language" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "dialect" ("created_at");
-CREATE INDEX "updated_at_index" ON "dialect" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "mediafile" ("created_at");
-CREATE INDEX "updated_at_index" ON "mediafile" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "expression_relation" ("created_at");
-
-CREATE INDEX "created_at_index" ON "relation_type" ("created_at");
-CREATE INDEX "updated_at_index" ON "relation_type" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "source" ("created_at");
-CREATE INDEX "updated_at_index" ON "source" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "written_source" ("created_at");
-CREATE INDEX "updated_at_index" ON "written_source" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "etymology" ("created_at");
-CREATE INDEX "updated_at_index" ON "etymology" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "case" ("created_at");
-CREATE INDEX "updated_at_index" ON "case" ("updated_at");
-
-CREATE INDEX "created_at_index" ON "declension" ("created_at");
-CREATE INDEX "updated_at_index" ON "declension" ("updated_at");
-
 CREATE INDEX exp_gin_idx ON expression USING gin (spelling gin_trgm_ops);
 -- endregion
 
 -- region Comments
-COMMENT ON COLUMN "example"."src_lang_id" IS 'Source language ID';
+COMMENT ON COLUMN example.src_lang_id IS 'Source language ID';
 
-COMMENT ON COLUMN "example"."trl_lang_id" IS 'Translation language ID';
+COMMENT ON COLUMN example.trl_lang_id IS 'Translation language ID';
 
-COMMENT ON COLUMN "example"."raw" IS 'String combination of source and translation';
+COMMENT ON COLUMN example.raw IS 'String combination of source and translation';
 
-COMMENT ON COLUMN "language"."id" IS 'iso639_3';
+COMMENT ON COLUMN language.id IS 'iso639_3';
 
-COMMENT ON COLUMN "language"."iso_2" IS 'iso639_2';
+COMMENT ON COLUMN language.iso_2 IS 'iso639_2';
 
-COMMENT ON COLUMN "written_source"."description" IS 'Tells about how the source obtained its information when necessary';
+COMMENT ON COLUMN written_source.description IS 'Tells about how the source obtained its information when necessary';
 
 COMMENT ON TABLE "case" IS 'Падеж';
 
-COMMENT ON COLUMN "case"."name" IS 'e.g. Nominative, Accusative etc.';
+COMMENT ON COLUMN "case".name IS 'e.g. Nominative, Accusative etc.';
 
-COMMENT ON COLUMN "case"."question" IS 'question to be answered by given case e.g. Who/What?, Whom/What? etc.';
+COMMENT ON COLUMN "case".question IS 'question to be answered by given case e.g. Who/What?, Whom/What? etc.';
 
-COMMENT ON COLUMN "case"."language_id" IS 'refers to a language where the given case belongs to';
+COMMENT ON COLUMN "case".language_id IS 'refers to a language where the given case belongs to';
 
-COMMENT ON TABLE "declension" IS 'Склонение';
+COMMENT ON TABLE declension IS 'Склонение';
 
-COMMENT ON COLUMN "declension"."expression_details_id" IS 'expression to decline according to the given case | слово для склонения согласно указанному падежу';
+COMMENT ON COLUMN declension.expression_details_id IS 'expression to decline according to the given case | слово для склонения согласно указанному падежу';
 
-COMMENT ON COLUMN "declension"."num" IS 'grammatical number e.g. single_1 for "доске" and plural_many for "досках"';
+COMMENT ON COLUMN declension.num IS 'grammatical number e.g. single_1 for "доске" and plural_many for "досках"';
 -- endregion
