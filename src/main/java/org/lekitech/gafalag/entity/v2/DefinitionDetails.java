4bb83bb6044502d29a1d4aab78f4dbc4f17af144
Kamran Tadzjibov <ktadzjibov@gmail.com>
2023-09-08 17:51:44 +0200

    fixed v1 language errors in v2 entities

--- src/main/java/org/lekitech/gafalag/entity/v2/DefinitionDetails.java
+++ src/main/java/org/lekitech/gafalag/entity/v2/DefinitionDetails.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.lekitech.gafalag.entity.v1.Language;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
