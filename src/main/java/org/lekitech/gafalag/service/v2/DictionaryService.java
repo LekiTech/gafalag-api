5825807a03d044d73a5d2a89fae1902589c43758
Enver Eskendarov <envereskendarov@gmail.com>
2024-01-05 10:30:36 -0800

    [WIP] code reformatting

--- src/main/java/org/lekitech/gafalag/service/v2/DictionaryService.java
+++ src/main/java/org/lekitech/gafalag/service/v2/DictionaryService.java
@@ -1,8 +1,8 @@
 package org.lekitech.gafalag.service.v2;
 
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.lekitech.gafalag.dto.v2.DefinitionDetailsDto;
+import org.lekitech.gafalag.dto.v2.DefinitionDto;
 import org.lekitech.gafalag.dto.v2.DictionaryDto;
 import org.lekitech.gafalag.dto.v2.ExampleDto;
 import org.lekitech.gafalag.dto.v2.ExpressionDetailsDto;
@@ -23,7 +23,6 @@ import org.lekitech.gafalag.entity.v2.Source;
 import org.lekitech.gafalag.entity.v2.Tag;
 import org.lekitech.gafalag.entity.v2.WrittenSource;
 import org.lekitech.gafalag.repository.v2.ExpressionDetailsRepositoryV2;
-import org.lekitech.gafalag.repository.v2.ExpressionMatchDetailsRepository;
 import org.lekitech.gafalag.repository.v2.ExpressionRepositoryV2;
 import org.lekitech.gafalag.repository.v2.LanguageRepositoryV2;
 import org.lekitech.gafalag.repository.v2.SourceRepositoryV2;
@@ -52,7 +51,6 @@ public class DictionaryService {
 
     public DictionaryService(ExpressionRepositoryV2 expressionRepositoryV2,
                              ExpressionDetailsRepositoryV2 expressionDetailsRepositoryV2,
-                             ExpressionMatchDetailsRepository expressionMatchDetailsRepository,
                              WrittenSourceRepository writtenSourceRepository,
                              SourceRepositoryV2 sourceRepositoryV2,
                              LanguageRepositoryV2 languageRepositoryV2,
@@ -65,15 +63,10 @@ public class DictionaryService {
         this.tagRepositoryV2 = tagRepositoryV2;
     }
 
-    public void save(ExpressionDto dto) {
-        expressionRepositoryV2.save(new Expression());
-    }
-
     @Transactional
     public void saveDictionary(DictionaryDto dto) {
         try {
-
-            val source = createAndSaveSource(dto);
+            final Source source = createAndSaveSource(dto);
             saveAllExpressionData(dto, source);
 
             log.info("Successfully saved dictionary data!");
@@ -83,8 +76,8 @@ public class DictionaryService {
     }
 
     private Source createAndSaveSource(DictionaryDto dto) {
-        val source = sourceRepositoryV2.save(new Source(Source.Type.WRITTEN));
-        val writtenSource = new WrittenSource(
+        final Source source = sourceRepositoryV2.save(new Source(Source.Type.WRITTEN));
+        final WrittenSource writtenSource = new WrittenSource(
                 source,
                 dto.name(),
                 dto.authors(),
@@ -96,15 +89,16 @@ public class DictionaryService {
                 dto.seeSourceURL(),
                 dto.description()
         );
+
         writtenSourceRepository.save(writtenSource);
         return source;
     }
 
     public void saveAllExpressionData(DictionaryDto dto, Source source) {
-        val expLang = languageRepositoryV2.getById(dto.expressionLanguageId());
-        val defLang = languageRepositoryV2.getById(dto.definitionLanguageId());
-
+        final Language expLang = languageRepositoryV2.getById(dto.expressionLanguageId());
+        final Language defLang = languageRepositoryV2.getById(dto.definitionLanguageId());
         final Map<String, List<ExpressionDetailsDto>> expressionDtoMap = new HashMap<>();
+
         for (final ExpressionDto expressionDto : dto.expressions()) {
             for (final String spelling : expressionDto.spelling()) {
                 List<ExpressionDetailsDto> detailsList = expressionDtoMap.getOrDefault(spelling, new ArrayList<>());
@@ -112,7 +106,10 @@ public class DictionaryService {
                 expressionDtoMap.put(spelling, detailsList);
             }
         }
-        expressionDtoMap.forEach((spelling, details) -> saveSingleExpression(spelling, details, expLang, source, defLang));
+
+        expressionDtoMap.forEach(
+                (spelling, details) -> saveSingleExpression(spelling, details, expLang, source, defLang)
+        );
     }
 
     private void saveSingleExpression(String spelling,
@@ -120,7 +117,8 @@ public class DictionaryService {
                                       Language expLang,
                                       Source source,
                                       Language defLang) {
-        val expressionEntity = expressionRepositoryV2.save(new Expression(spelling, expLang));
+        final Expression expressionEntity = expressionRepositoryV2.save(new Expression(spelling, expLang));
+
         expressionDetailsRepositoryV2.saveAll(
                 createExpressionDetails(source, expLang, defLang, expressionDetailsDtoList, expressionEntity)
         );
@@ -132,28 +130,33 @@ public class DictionaryService {
                                                             List<ExpressionDetailsDto> expressionDetailsDtoList,
                                                             Expression expressionEntity) {
         final List<ExpressionDetails> expressionDetailsEntities = new ArrayList<>();
-        for (ExpressionDetailsDto expressionDetailsDto : expressionDetailsDtoList) {
+
+        for (final ExpressionDetailsDto expressionDetailsDto : expressionDetailsDtoList) {
             final ExpressionDetails expressionDetailsEntity = new ExpressionDetails(
                     expressionDetailsDto.gr(),
                     expressionDetailsDto.inflection(),
                     source
             );
+
             if (expressionDetailsDto.examples().isPresent()) {
                 final List<ExpressionExample> expressionExampleEntities = createExpressionExampleList(
                         expressionDetailsDto.examples().get(), expressionDetailsEntity, expLang, defLang
                 );
                 expressionDetailsEntity.addExpressionExamples(expressionExampleEntities);
             }
+
             final List<DefinitionDetails> definitionDetailsEntities = new ArrayList<>();
-            for (DefinitionDetailsDto definitionDetailsDto : expressionDetailsDto.definitionDetails()) {
+            for (final DefinitionDetailsDto definitionDetailsDto : expressionDetailsDto.definitionDetails()) {
                 final DefinitionDetails definitionDetailEntity = createDefinitionDetails(
                         expLang, defLang, expressionDetailsEntity, definitionDetailsDto
                 );
                 definitionDetailsEntities.add(definitionDetailEntity);
             }
+
             expressionDetailsEntity.addDefinitionDetails(definitionDetailsEntities);
-            expressionDetailsEntity.getExpressionMatchDetails()
-                    .add(new ExpressionMatchDetails(expressionEntity, expressionDetailsEntity));
+            expressionDetailsEntity.getExpressionMatchDetails().add(
+                    new ExpressionMatchDetails(expressionEntity, expressionDetailsEntity)
+            );
             expressionDetailsEntities.add(expressionDetailsEntity);
         }
         return expressionDetailsEntities;
@@ -166,28 +169,37 @@ public class DictionaryService {
         final DefinitionDetails definitionDetailEntity = new DefinitionDetails(expressionDetailsEntity, defLang);
         final List<Definition> definitionEntities = createDefinitionList(defDetail, definitionDetailEntity);
         definitionDetailEntity.addDefinitions(definitionEntities);
+
         if (defDetail.examples().isPresent()) {
             final List<DefinitionExample> definitionExampleList = createDefinitionExampleList(
-                    defDetail.examples().get(), definitionDetailEntity, expLang, defLang
+                    defDetail.examples().get(),
+                    definitionDetailEntity,
+                    expLang,
+                    defLang
             );
             definitionDetailEntity.addDefinitionExamples(definitionExampleList);
         }
+
         if (defDetail.tags().isPresent()) {
             final List<DefinitionDetailsTag> definitionDetailsTagEntities = createDefinitionDetailsTags(
-                    defDetail.tags().get(), definitionDetailEntity
+                    defDetail.tags().get(),
+                    definitionDetailEntity
             );
             definitionDetailEntity.addDefinitionDetailsTags(definitionDetailsTagEntities);
         }
+
         return definitionDetailEntity;
     }
 
     private List<DefinitionDetailsTag> createDefinitionDetailsTags(List<String> tags,
                                                                    DefinitionDetails definitionDetailEntity) {
         final List<DefinitionDetailsTag> definitionDetailsTagEntities = new ArrayList<>();
-        for (String defDetailTag : tags) {
+
+        for (final String defDetailTag : tags) {
             final Tag tagEntity = getOrCreateTag(defDetailTag);
             final DefinitionDetailsTag definitionDetailsTagEntity = new DefinitionDetailsTag(
-                    tagEntity, definitionDetailEntity
+                    tagEntity,
+                    definitionDetailEntity
             );
             definitionDetailsTagEntities.add(definitionDetailsTagEntity);
         }
@@ -199,9 +211,17 @@ public class DictionaryService {
                                                                 Language expLang,
                                                                 Language defLang) {
         final List<ExpressionExample> expressionExampleList = new ArrayList<>();
-        for (ExampleDto exampleDto : examples) {
-            final Example exampleEntity = createExample(expLang, defLang, exampleDto);
-            final ExpressionExample expressionExample = new ExpressionExample(expressionDetailsEntity, exampleEntity);
+
+        for (final ExampleDto exampleDto : examples) {
+            final Example exampleEntity = createExample(
+                    expLang,
+                    defLang,
+                    exampleDto
+            );
+            final ExpressionExample expressionExample = new ExpressionExample(
+                    expressionDetailsEntity,
+                    exampleEntity
+            );
             expressionExampleList.add(expressionExample);
         }
         return expressionExampleList;
@@ -212,15 +232,25 @@ public class DictionaryService {
                                                                 Language expLang,
                                                                 Language defLang) {
         final List<DefinitionExample> definitionExampleList = new ArrayList<>();
-        for (ExampleDto exampleDto : examples) {
-            final Example exampleEntity = createExample(expLang, defLang, exampleDto);
-            final DefinitionExample definitionExample = new DefinitionExample(exampleEntity, definitionDetailsEntity);
+
+        for (final ExampleDto exampleDto : examples) {
+            final Example exampleEntity = createExample(
+                    expLang,
+                    defLang,
+                    exampleDto
+            );
+            final DefinitionExample definitionExample = new DefinitionExample(
+                    exampleEntity,
+                    definitionDetailsEntity
+            );
             definitionExampleList.add(definitionExample);
         }
         return definitionExampleList;
     }
 
-    private Example createExample(Language expLang, Language defLang, ExampleDto exampleDto) {
+    private Example createExample(Language expLang,
+                                  Language defLang,
+                                  ExampleDto exampleDto) {
         final Example exampleEntity = new Example(
                 exampleDto.src(),
                 exampleDto.trl(),
@@ -229,29 +259,43 @@ public class DictionaryService {
                 exampleDto.raw()
         );
         if (exampleDto.tags().isPresent()) {
-            final List<ExampleTag> exampleTagEntities = createExampleTags(exampleDto.tags().get(), exampleEntity);
+            final List<ExampleTag> exampleTagEntities = createExampleTags(
+                    exampleDto.tags().get(),
+                    exampleEntity
+            );
             exampleEntity.addExampleTags(exampleTagEntities);
         }
         return exampleEntity;
     }
 
-    private List<ExampleTag> createExampleTags(List<String> tagsDto, Example exampleEntity) {
+    private List<ExampleTag> createExampleTags(List<String> tagsDto,
+                                               Example exampleEntity) {
         final List<ExampleTag> exampleTagEntities = new ArrayList<>();
-        for (val defDetailExampleTag : tagsDto) {
+
+        for (final String defDetailExampleTag : tagsDto) {
             final Tag tagEntity = getOrCreateTag(defDetailExampleTag);
-            final ExampleTag exampleTagEntity = new ExampleTag(tagEntity, exampleEntity);
+            final ExampleTag exampleTagEntity = new ExampleTag(
+                    tagEntity,
+                    exampleEntity
+            );
             exampleTagEntities.add(exampleTagEntity);
         }
         return exampleTagEntities;
     }
 
-    private List<Definition> createDefinitionList(DefinitionDetailsDto defDetail, DefinitionDetails definitionDetailEntity) {
+    private List<Definition> createDefinitionList(DefinitionDetailsDto defDetail,
+                                                  DefinitionDetails definitionDetailEntity) {
         final List<Definition> definitionEntities = new ArrayList<>();
-        for (val definition : defDetail.definitions()) {
-            final Definition definitionEntity = new Definition(definition.value(), definitionDetailEntity);
+
+        for (final DefinitionDto definition : defDetail.definitions()) {
+            final Definition definitionEntity = new Definition(
+                    definition.value(),
+                    definitionDetailEntity
+            );
             if (definition.tags().isPresent()) {
                 final List<DefinitionTag> definitionTagList = createDefinitionTagList(
-                        definition.tags().get(), definitionEntity
+                        definition.tags().get(),
+                        definitionEntity
                 );
                 definitionEntity.addDefinitionTags(definitionTagList);
             }
@@ -260,11 +304,16 @@ public class DictionaryService {
         return definitionEntities;
     }
 
-    private List<DefinitionTag> createDefinitionTagList(List<String> definitionTags, Definition definitionEntity) {
+    private List<DefinitionTag> createDefinitionTagList(List<String> definitionTags,
+                                                        Definition definitionEntity) {
         final List<DefinitionTag> definitionTagEntities = new ArrayList<>();
-        for (String definitionTag : definitionTags) {
+
+        for (final String definitionTag : definitionTags) {
             final Tag tagEntity = getOrCreateTag(definitionTag);
-            final DefinitionTag definitionTagEntity = new DefinitionTag(tagEntity, definitionEntity);
+            final DefinitionTag definitionTagEntity = new DefinitionTag(
+                    tagEntity,
+                    definitionEntity
+            );
             definitionTagEntities.add(definitionTagEntity);
         }
         return definitionTagEntities;
